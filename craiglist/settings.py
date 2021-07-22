ea3c0926309932c17f3d63e5fe3c298eed7bfe44
Jumba Mark <jumbamark@yahoo.com>
2021-07-22 23:08:52 +0300

    Deploying on heroku

--- craiglist/settings.py
+++ craiglist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,8 @@ STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+
+
+
